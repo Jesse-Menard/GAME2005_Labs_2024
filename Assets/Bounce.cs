5e0f81f348db8e10176ae05f03692dcd8fcb80bf
Jesse Menard <Jessemenard04@hotmail.com>
2024-09-12 16:32:58 -0400

    Tweaks to make code work better

--- Assets/Bounce.cs
+++ Assets/Bounce.cs
@@ -4,39 +4,45 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    // public float Xpos, Ypos = 0;
     public float mass = 1.0f;
     public float gravity = -1.0f;
     public float friction = 0.98f;
 
-    public Vector3 Vel = new Vector3(0.0f,0.0f,0.0f);
+    public Vector3 vel = new Vector3(0.0f, 0.0f, 0.0f);
     float time = 0;
+    bool applyFric = false;
     
     private void FixedUpdate()
     {
         float deltaT = 1.0f / 60.0f;
 
-        if (friction > 1.0f) {friction = 1.0f} // gets funky when over 1
-
-        // if/else prevents passively lowering position when reversing vel
-        if (transform.position.y <= 0)
-        {
-            Vel *= -1.0f;
-        }
-        else
-        {
-            Vel.y += mass * gravity * deltaT;
-            Vel *= friction;
-        }
-
-        // Prevent infinite bounce
+        // Prevent dampen when low to prevent infinite
         if (transform.position.y <= mass * -gravity * deltaT * friction && 
-            Vel.y <= mass * -gravity * deltaT * friction)
+            vel.magnitude <= mass * -gravity * deltaT)
         {
-            Vel.y *= 0.75f;
+            print("DAMPEN");
+            vel.y *= 0.75f;
+        }
+        // If next change would make neg, flip instead
+        else if (transform.position.y + vel.y < 0.0f)
+        {
+            transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+            vel *= -1.0f;
+            applyFric = true;
+        }         
+        // gravity & friction
+        else
+        {            
+            vel.y += mass * gravity * deltaT;
+           
+            if (applyFric)
+            {
+                vel *= friction;
+                applyFric = false;
+            }
         }
 
-        transform.position += Vel;
+        transform.position += vel;
         
         time += deltaT;
     }
