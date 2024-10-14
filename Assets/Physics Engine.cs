b91d4ebdd498937f8fa02aa7256de25d5e8ce3e3
Jesse Menard <Jessemenard04@hotmail.com>
2024-10-13 23:02:00 -0400

    added collision manager

--- Assets/Physics Engine.cs
+++ Assets/Physics Engine.cs
@@ -55,37 +55,5 @@ public class PhysicsEngine : MonoBehaviour
 
             time += dt;
         }
-
-        foreach (PhysicsObject object1 in physicsObjects)
-        {
-            object1.GetComponent<Renderer>().material.color = Color.white;
-        }
-        
-        for(int a = 0; a < physicsObjects.Count; a++)
-        {
-            PhysicsObject object1 = physicsObjects[a];
-
-            for(int b = a + 1; b < physicsObjects.Count; b++)
-            {
-                PhysicsObject object2 = physicsObjects[b];
-
-                if (object1.GetType() == typeof(Sphere) && object2.GetType() == typeof(Sphere))
-                {
-                    if (SphereSphereCollision(object1 as Sphere, object2 as Sphere))
-                    {
-                        // Colliding
-
-                        object1.GetComponent<Renderer>().material.color = new Color(180f / 255f, 0f, 1f);
-                        object2.GetComponent<Renderer>().material.color = new Color(180f / 255f, 0f, 1f);
-                    }
-                }
-            }
-        }
-    }
-
-    bool SphereSphereCollision(Sphere ob1, Sphere ob2)
-    {
-        float distance = (ob1.transform.position - ob2.transform.position).magnitude;
-        return distance < (ob1.radius + ob2.radius);
     }
 }
